847de86807d4261d99d2f50a0f95ce1f19ff9cc1
kreikrei <kreitonstrs@gmail.com>
2021-01-25 19:59:08 +0700

    add node,bound,stabilizer to structs

--- src/Structures.jl
+++ src/Structures.jl
@@ -46,14 +46,55 @@ struct veh
     fd::Float64
 end
 
-struct status
-    #FOR ASSESSING A DATASET
-    number_of_vertices::Int64
-    number_of_vehicles::Int64
-    unique_vertices_type::Vector{String}
-    unique_vehicle_types::Vector{String}
-    vertex_type_breakdown::Dict{String,Vector{Int64}}
-    vehicle_type_breakdown::Dict{String,Vector{Int64}}
-    unique_cover::Vector{Int64}
-    unique_loadp::Vector{Int64}
+struct dt
+    V::Dict{Int64,vtx}
+    dist::JuMP.Containers.DenseAxisArray
+    K::Dict{Int64,veh}
+    T::Vector{Int64}
+    d::JuMP.Containers.DenseAxisArray
+end
+
+struct col
+    #q related
+    q::JuMP.Containers.DenseAxisArray
+    u::JuMP.Containers.DenseAxisArray
+    v::JuMP.Containers.DenseAxisArray
+    l::JuMP.Containers.DenseAxisArray
+
+    #0-1 related
+    p::JuMP.Containers.DenseAxisArray
+    y::JuMP.Containers.DenseAxisArray
+    z::JuMP.Containers.DenseAxisArray
+    x::JuMP.Containers.DenseAxisArray
+end
+
+struct bound
+    idx::NamedTuple
+    val::Int64
+end
+
+struct stabilizer
+    slCoeff::Float64
+    suCoeff::Float64
+    slLim::JuMP.Containers.DenseAxisArray
+    suLim::JuMP.Containers.DenseAxisArray
+end
+
+struct node
+    #IDENTIFIER
+    parent::UUID
+    self::UUID
+
+    #BUILD COMPONENT
+    base::dt
+
+    #Dynamaic SET
+    bounds::Vector{bound}
+    columns::Vector{col}
+
+    #SUPPORT
+    stab::stabilizer
+
+    #STATUS
+    status::Vector{String}
 end
