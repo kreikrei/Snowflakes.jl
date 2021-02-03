583e0e8eaf2b5e622e5d0dc601d4e169578b36a2
kreikrei <kreitonstrs@gmail.com>
2021-02-03 18:33:08 +0700

    styling for settings.jl

--- src/Settings.jl
+++ src/Settings.jl
@@ -3,21 +3,15 @@
 # =========================================================================
 
 const default_optimizer = Ref{Any}(nothing)
-
-set_default_optimizer!(O) = default_optimizer[] = O
-
-get_default_optimizer() = default_optimizer[]
-
-reset_default_optimizer() = default_optimizer[] = nothing
-
 const slack_coeff = Ref{Any}(nothing)
-
-set_slack_coeff!(O) = slack_coeff[] = O
-
-sl_C() = slack_coeff[]
-
 const surp_coeff = Ref{Any}(nothing)
 
-set_surp_coeff!(O) = surp_coeff[] = O
+set_default_optimizer!(O) = default_optimizer[] = O
+get_default_optimizer() = default_optimizer[]
+reset_default_optimizer() = default_optimizer[] = nothing
 
+set_slack_coeff!(O) = slack_coeff[] = O
+sl_C() = slack_coeff[]
+
+set_surp_coeff!(O) = surp_coeff[] = O
 su_C() = surp_coeff[]
